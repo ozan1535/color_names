50c008cea0f5a67f06f5b5923b134e252cc53df7
Ozan <ozanbilgic1535@gmail.com>
2023-11-29 22:24:15 +0100

    Create app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { RgbColorPicker } from "react-colorful";
+import "./App.css";
 
 function App() {
+  const [color, setColor] = useState({ r: 200, g: 150, b: 35 });
+  const [colorName, setColorName] = useState("");
+  const colorRange = 200;
+  const canChangeColor =
+    color.r > colorRange && color.g > colorRange && color.b > colorRange;
+  useEffect(() => {
+    fetch(
+      `https://www.thecolorapi.com/id?rgb=${color.r},${color.g},${color.b}&format=json`
+    )
+      .then((response) => response.json())
+      .then((data) => setColorName(data.name.value));
+  }, [color]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{ background: `rgb(${color.r},${color.g},${color.b})` }}
+    >
+      <RgbColorPicker color={color} onChange={setColor} />
+      <h3 style={{ color: canChangeColor ? "currentcolor" : "white " }}>
+        Color Name: {colorName}
+      </h3>
     </div>
   );
 }
